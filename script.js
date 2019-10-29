221070639f7fe5237f566d36097ba6264056add7
TonyV <tvester1487@gmail.com>
2019-10-29 17:48:02 -0500

    link 5 day call

--- script.js
+++ script.js
@@ -10,18 +10,56 @@ $(document).ready(function() {
       url: queryUrl,
       method: 'GET'
     }).then(function(response) {
-      console.log(response);
       $('#cityName').text(response.name);
-      $('#temp').text(`Temperature: ` + response.main.temp);
-      $('#humidity').text(`Humidity: ` + response.main.humidity);
-      $('#windSpeed').text(`Wind Speed: ` + response.wind.speed);
-      $('#uvIndex').text(`UV Index; `);
-      console.log(response.name);
+      $('#temp').text(`Temperature: ` + response.main.temp + ' °F');
+      $('#humidity').text(`Humidity: ` + response.main.humidity + '%');
+      $('#windSpeed').text(`Wind Speed: ` + response.wind.speed + ' MPH');
+      // Create UV index URL query
+      // Pull coordinates from the returned OBJ and pass coordinates into UV index API call
+
+      var queryUrlUV =
+        'http://api.openweathermap.org/data/2.5/uvi?appid=' +
+        apiKey +
+        '&lat=' +
+        response.coord.lat +
+        '&lon=' +
+        response.coord.lon;
+
+      // Create ajax call to UV index URL
+
+      $.ajax({
+        url: queryUrlUV,
+        method: 'GET'
+      }).then(function(responseUV) {
+        $('#uvIndex').text(responseUV.value);
+      });
+    });
+  }
+
+  // Setup call for 5 day forecast
+
+  function fiveDayForecast(city) {
+    var queryUrl5day =
+      'http://api.openweathermap.org/data/2.5/forecast?q=' +
+      city +
+      '&units=imperial&appid=' +
+      apiKey;
+    $.ajax({
+      url: queryUrl5day,
+      method: 'GET'
+    }).then(function(response) {
+      console.log(response);
     });
   }
 
   $('#button-addon2').on('click', function() {
     cityName = $('#citySearch').val();
     currentWeather(cityName);
+    fiveDayForecast(cityName);
+  });
+  $('li').on('click', function() {
+    cityName = $(this).text();
+    currentWeather(cityName);
+    fiveDayForecast(cityName);
   });
 });
